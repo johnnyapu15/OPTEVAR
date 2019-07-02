ee6d851eba5a906cc743771ae5507d23b16fac64
johnnyapu15 <johnnyapu15@gmail.com>
2019-07-02 17:31:36 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,24 @@
+# 1. Read nodes, edges from environment json
+## It contains values for calculating RSET/ASET - for example area of a room(node)
+## Initiate edges weigths with calculating distances between nodes
+
+# 2. Read scenerio parameters - maximum number of people, avg velocity of movement, ...
+
+# 3. Read realtime environment data from json - sensor data(number of people on a room, fire on/off ...)
+
+# 4. Calculate weights of nodes
+
+# 5. Calculate the optimal evacuation route using dijkstra's algorithm
+## using networkx?
+
+
+#############################
+
+import json
+
+
+
+# 1. Read envs
+env_route = "./env/testenv.json"
+env = json.load(env_route)
+
